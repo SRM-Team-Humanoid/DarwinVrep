73d9eeb905a2869716feae0799e81f01f5f963d9
WarrG3X <warrier.abhishek@gmail.com>
2018-01-12 01:29:00 +0530

    Motor Test

--- DarwinVrep.py
+++ DarwinVrep.py
@@ -12,22 +12,27 @@ else:
     print "Connection Failed"
     sys.exit()
 
-errCode,motor = vrep.simxGetObjectHandle(ClientID,'j_pan',vrep.simx_opmode_oneshot_wait)
+errCode,motor1 = vrep.simxGetObjectHandle(ClientID,'j_tibia_r',vrep.simx_opmode_oneshot_wait)
+errCode,motor2 = vrep.simxGetObjectHandle(ClientID,'j_tibia_l',vrep.simx_opmode_oneshot_wait)
 
-print motor
+print motor1
+print motor2
 
 
 while True:
     for i in range(90):
         angle = math.radians(i)
-        vrep.simxSetJointTargetPosition(ClientID,motor,angle,vrep.simx_opmode_oneshot_wait)
+        vrep.simxSetJointTargetPosition(ClientID,motor1,angle,vrep.simx_opmode_oneshot_wait)
+        vrep.simxSetJointTargetPosition(ClientID,motor2,-angle,vrep.simx_opmode_oneshot_wait)
         time.sleep(0.001)
     for i in range(90,-90,-1):
         angle = math.radians(i)
-        vrep.simxSetJointTargetPosition(ClientID,motor,angle,vrep.simx_opmode_oneshot_wait)
+        vrep.simxSetJointTargetPosition(ClientID,motor1,angle,vrep.simx_opmode_oneshot_wait)
+        vrep.simxSetJointTargetPosition(ClientID,motor2,-angle,vrep.simx_opmode_oneshot_wait)
         time.sleep(0.001)
     for i in  range(-90,0):
         angle = math.radians(i)
-        vrep.simxSetJointTargetPosition(ClientID,motor,angle,vrep.simx_opmode_oneshot_wait)
+        vrep.simxSetJointTargetPosition(ClientID,motor1,angle,vrep.simx_opmode_oneshot_wait)
+        vrep.simxSetJointTargetPosition(ClientID,motor2,-angle,vrep.simx_opmode_oneshot_wait)
         time.sleep(0.001)
 
